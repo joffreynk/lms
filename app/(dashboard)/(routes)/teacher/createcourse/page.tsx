5beab069696995080f66e98e9828cda7e4278942
joffreynk <joyjoffrey@gmail.com>
2023-10-09 23:24:01 +0200

    add course title

--- app/(dashboard)/(routes)/teacher/createcourse/page.tsx
+++ app/(dashboard)/(routes)/teacher/createcourse/page.tsx
@@ -1,39 +1,95 @@
-'use client'
+"use client";
 
-import * as z from 'zod';
-import {zodResolver} from '@hookform/resolvers/zod';
-import {useForm} from 'react-hook-form';
-import { useRouter } from 'next/navigation';
-import useSWR from 'swr'
-import { Button } from '@/components/ui/button';
-import { Form, FormControl, FormDescription, FormField, FormLabel, FormMessage } from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import Link from "next/link";
+import toast from "react-hot-toast";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 const formSchema = z.object({
   title: z.string().min(1, {
-    message: 'Title is required',
+    message: "Title is required",
   }),
 });
 
 const CreateCourse = () => {
-  const form = useForm<z.infer <typeof formSchema>>({
+  const router  = useRouter();
+  const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       title: "",
     },
   });
 
-  const {isSubmitting, isValid} = form.formState
+  const { isSubmitting, isValid } = form.formState;
 
-  const onSubmit = (values: z.infer <typeof formSchema>) => {
-    console.log(values);
-    
-  }
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+   try {
+    const response  = await axios.post('/api/courses', values);
+    router.push(`teacher/courses/${response.data.id}`);
+   } catch (error) {
+    toast.error('Something went wrong')
+   }
+  };
   return (
-    <div className="">
-      Create Course
+    <div className="max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6">
+      <div className="">
+        <h1 className="text-2xl">Name your New Course</h1>
+        <p className="text-sm text-slate-600">
+          What would you like to name your new course? You can change it later!
+        </p>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8 mt-8"
+          >
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Course title</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder='e.g "Introduction to CSS"'
+                      {...field}
+                      className="outline-none"
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    your title fits what you will teach
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <div className="flex items-center gap-x-2">
+                <Link href='/teacher/courses'>
+                  <Button type="button" variant='ghost'>Cancel</Button>
+                </Link>
+                <Button type="submit" disabled={!isValid || isSubmitting}>Continue</Button>
+            </div>
+          </form>
+        </Form>
+      </div>
     </div>
   );
-}
+};
 
-export default CreateCourse
+export default CreateCourse;
