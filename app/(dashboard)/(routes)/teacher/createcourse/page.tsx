2ad690d01e18102602317f7f34cff7226f4bec54
joffreynk <joyjoffrey@gmail.com>
2023-10-09 21:28:01 +0200

    add form schema and its properties

--- app/(dashboard)/(routes)/teacher/createcourse/page.tsx
+++ app/(dashboard)/(routes)/teacher/createcourse/page.tsx
@@ -9,7 +9,26 @@ import { Button } from '@/components/ui/button';
 import { Form, FormControl, FormDescription, FormField, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 
+const formSchema = z.object({
+  title: z.string().min(1, {
+    message: 'Title is required',
+  }),
+});
+
 const CreateCourse = () => {
+  const form = useForm<z.infer <typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+    },
+  });
+
+  const {isSubmitting, isValid} = form.formState
+
+  const onSubmit = (values: z.infer <typeof formSchema>) => {
+    console.log(values);
+    
+  }
   return (
     <div className="">
       Create Course
